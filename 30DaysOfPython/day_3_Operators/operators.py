710487782a507cec74a77f997f8f48634f52a6bc
Albookman <atorresjr@proton.me>
2026-03-17 07:19:51 -0300

    início do 3º dia

---
+++ 30DaysOfPython/day_3_Operators/operators.py
@@ -0,0 +1,48 @@
+# Declaring the variable at the top first
+
+a = 3
+b = 2
+
+#Aritimetc operations and assigning the result to a veriable
+
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+print ("a + b = ", total)
+print (" a - b = ", diff)
+print ("a * b = " , product)
+print ("a / b = ", division)
+print ("a % b = ", remainder)
+print ("a // b = ", floor_division)
+print ("a ** b = ", exponential)
+
+print('== Addition, Subtraction, Multiplication, Division, Modulus ==')
+
+# Declaring values and organizing them together
+num_one = 3
+num_two = 4
+
+# Arithmetic operations
+
+total = num_one + num_two 
+diff = num_two - num_one
+product = num_one * num_two
+division = num_two / num_one
+remainder = num_two % num_one
+
+print ("Soma" , total)
+print ("Subtração , " , diff)
+print ("Multiplicação , ", product)
+print ("Divisão ," , division)
+print ("Resto da Divisão , ", remainder)
+
+#Calculando a área de um círculo
+
+raio = 10
+circle_area = 3.14 * raio**2
+print ("Área do Círculo = ", circle_area)
